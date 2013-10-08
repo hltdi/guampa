0222d52a706083f1b71cb30b43f03b5d2ad0fabb
SirSkidmore <taylor@taylorskidmore.com>
2013-10-08 18:07:44 -0400

    added really bad MVP to testing folder

---
+++ testing/app.js
@@ -0,0 +1,16 @@
+function translateCtrl($scope) {
+    $scope.editedItem = null;
+    $scope.paragraph = [{ content: "My cat is brown", editing: false },
+                        { content: "My dog is black", editing: false },
+                        { content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", editing: false}];
+
+    $scope.startEditing = function(sentence) {
+        sentence.editing = true;
+        $scope.editedItem = sentence;
+    }
+
+    $scope.doneEditing = function(sentence) {
+        sentence.editing = false;
+        $scope.editedItem = null;
+    }
+}
