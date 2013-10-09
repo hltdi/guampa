36de8fe233b4bdc0c125f7aae92328dbf7312c25
Alex Rudnick <alex.rudnick@gmail.com>
2013-10-09 18:23:21 -0400

    make extract_vital_titles add tags to its output -- we're going to have to save these in the db

--- wikipedia-import/extract_vital_titles.py
+++ wikipedia-import/extract_vital_titles.py
@@ -4,15 +4,31 @@ import argparse
 import urllib.parse
 
 import bs4
+import sys
 
-def dump_links(html, outfile):
-    soup = bs4.BeautifulSoup(html)
-    for link in soup.find_all('a'):
-        target = link.get('href')
-        if target and target.startswith("/wiki/") and ":" not in target:
-            title = urllib.parse.unquote(target)[6:]
-            title = title.replace("_", " ")
-            print(title, file=outfile)
+def walk_the_soup(soup, outfile):
+    """Go through the page looking for links and section headings. Remember the
+    section headings and use them as tags!"""
+    for node in soup.find_all(['a', 'h2', 'h3']):
+        if node.name == 'h2':
+            h2 = None
+            h3 = None
+            children = node.findChildren('span')
+            if children: 
+                h2 = children[0].text
+        if node.name == 'h3':
+            children = node.findChildren('span')
+            if children: 
+                h3 = children[0].text
+        elif node.name == 'a':
+            target = node.get('href')
+            if target and target.startswith("/wiki/") and ":" not in target:
+                title = urllib.parse.unquote(target)[6:]
+                title = title.replace("_", " ")
+                tags = [h2]
+                if h3:
+                    tags.append(h3)
+                print("|||".join([title] + tags), file=outfile)
 
 def get_argparser():
     """Build the argument parser for main."""
@@ -27,7 +43,8 @@ def main():
 
     with open(args.infn) as infile, open(args.outfn, "w") as outfile:
         html = infile.read()
-        dump_links(html, outfile)
+        soup = bs4.BeautifulSoup(html)
+        walk_the_soup(soup, outfile)
 
 if __name__ == '__main__':
     main()
