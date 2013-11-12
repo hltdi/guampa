29fc065a7299a9f60c2a8c986d1d748e910f3ac3
Alex Rudnick <alex.rudnick@gmail.com>
2013-11-12 17:40:03 -0500

    add request to our virtualenv

--- scripts/mac_setup_virtualenv.sh
+++ scripts/mac_setup_virtualenv.sh
@@ -6,6 +6,7 @@ pip install Flask
 pip install sqlalchemy
 pip install beautifulsoup4
 pip install python-social-auth
+pip install requests
 
 pip install https://github.com/nltk/nltk/tarball/master
 ## make sure we have sentence segmenter models
